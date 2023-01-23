4a442c017e495ea06719ddf12a4a8165e7fb932a
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-01-23 16:03:17 +0100

    errors fixed

--- currency-contener/JS/script.js
+++ currency-contener/JS/script.js
@@ -1,10 +1,7 @@
 {
 
-
     const headingElement = document.querySelector(".js-heading");
 
-
-    
     const onFormSubmit = (event) => {
         event.preventDefault();
 
@@ -17,18 +14,16 @@
         const euroElement = document.querySelector(".js-euro");
 
 
+        const euro = euroElement.value;
+        const pln = plnElement.value;
 
         const result = euro * pln;
         resultElement.innerText = result.toFixed(2);
 
     };
-const CalculateResult =(euro,pln) =>{
-        const euro = euroElement.value;
-        const pln = plnElement.value;}
+
     const init = () => {
-
-        headingElement.addEventListener("submit", onFormSubmit);
-
+        headingElement.addEventListener("submit", onFormSubmit)
     };
 
     init();
